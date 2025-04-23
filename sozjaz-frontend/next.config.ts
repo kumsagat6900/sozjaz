923568603ba839317721ada5a8bbaf27761ac916
kumsagat <talgatworksnotion@gmail.com>
2025-04-23 14:39:25 +0500

    Initial commit with backend and frontend folders

--- sozjaz-frontend/next.config.ts
+++ sozjaz-frontend/next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/sozjaz-project-online', // Имя репозитория
-  trailingSlash: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
